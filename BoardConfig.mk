309239ca163ec8c4870da95c3abb5ea7d65541c3
burstlam <burstlam@gmail.com>
2014-10-19 00:08:29 +0800

    Disable periodic battery polling

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -17,6 +17,9 @@
 # Inherit from MSM8974 common
 -include device/oppo/msm8974-common/BoardConfigCommon.mk
 
+# Disable periodic battery polling
+HEALTHD_CHATTY_MODE := false
+
 TARGET_GCC_VERSION_AND := 4.9-sm
 TARGET_GCC_VERSION_ARM := 4.7
 TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.7
